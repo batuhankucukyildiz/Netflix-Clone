29765c4dc518e3939cbbc8272c58de41f8fac080
Batuhan <kucukyildizbatuhan@gmail.com>
2023-10-06 20:20:26 +0300

    first commit

---
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  Netflix Clone
+//
+//  Created by Batuhan Küçükyıldız on 6.10.2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+        
+    }
+
+}
---
+++ Netflix Clone/Controllers/UpComingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UpComingViewController.swift
+//  Netflix Clone
+//
+//  Created by Batuhan Küçükyıldız on 6.10.2023.
+//
+
+import UIKit
+
+class UpComingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
